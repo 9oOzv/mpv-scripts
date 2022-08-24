cee9ca7fc1228266d6199ff4ec74ab6a9adb0031
Miika Piiroinen <miika.piiroinen@gmail.com>
2022-08-24 23:24:09 +0300

    update prints

--- scripts/prints.lua
+++ scripts/prints.lua
@@ -5,10 +5,37 @@ local overlay = mp.create_osd_overlay("ass-events")
 local names = {
     'vf',
     'files',
+    'current-tracks',
 }
 local enabled = {}
 local f = {}
 local update_timer = nil
+
+function dump(o, pretty, indentation)
+    indentation = indentation or 0
+    pretty = pretty or false
+    if pretty then
+        if type(o) == 'table' then
+           local s = string.rep("\\h\\h", indentation) .. "\\{ " .. "\n"
+           for k,v in pairs(o) do
+             s = s .. string.rep("\\h\\h", indentation + 1) .. k .. ": " .. dump(v, pretty, indentation + 1) .. ",\n"
+           end
+           return s .. string.rep("\\h\\h", indentation) .. "\\}"
+        else
+           return tostring(o)
+        end
+    else
+        if type(o) == 'table' then
+           local s = "\\{ "
+           for k,v in pairs(o) do
+             s = s .. k .. ": " .. dump(v, pretty) .. ", "
+           end
+           return s .. "\\}"
+        else
+           return tostring(o)
+        end
+    end
+end
 
 f['vf'] = function(data)
     data = data .. "# FILTERS" .. "\n"
@@ -25,6 +52,19 @@ f['files'] = function(data)
     data = data .. "current-tracks/sub2/external-filename: " .. (mp.get_property("current-tracks/sub2/external-filename") or '')  .. "\n"
     data = data .. "path: " .. (mp.get_property("path") or '')  .. "\n"
     data = data .. "stream-open-filename: " .. (mp.get_property("stream-open-filename") or '')  .. "\n"
+    return data
+end
+
+f['current-tracks'] = function(data)
+    data = data .. "# CURRENT TRACKS" .. "\n"
+    data = data .. "video:" .. "\n"
+    data = data .. dump(mp.get_property_native("current-tracks/video")) .. "\n"
+    data = data .. "audio:" .. "\n"
+    data = data .. dump(mp.get_property_native("current-tracks/audio")) .. "\n"
+    data = data .. "sub:" .. "\n"
+    data = data .. dump(mp.get_property_native("current-tracks/sub")) .. "\n"
+    data = data .. "sub2:" .. "\n"
+    data = data .. dump(mp.get_property_native("current-tracks/sub2")) .. "\n"
     return data
 end
 
@@ -62,31 +102,40 @@ function update()
 end
 
 function toggle(name)
+    print("toggle " .. name)
     enabled[name] = not enabled[name]
     update_overlay()
 end
 
 function enable(name)
+    print("enable " .. name)
     enabled[name] = true
     update()
 end
 
 function disable(name)
+    print("disable " .. name)
     enabled[name] = nil
 end
 
 function disable_all()
-    enabled = {}
+    print("disable all")
+    for _, v in pairs(names) do
+        disable(v)
+    end
+    update()
     kill_update_timer()
 end
 
 function enable_all()
+    print("enable all")
     for _, v in pairs(names) do
         enable(v)
     end
 end
 
 function toggle_all()
+    print("toggle all")
     if update_timer then
         disable_all()
     else
